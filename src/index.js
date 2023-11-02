9f218307409fc858e72f187de6bfbd72663d6c7f
Zeckszan <yvonneengxinyee@gmail.com>
2023-11-02 21:33:31 +0800

    video transcribe

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports'; // Import your existing aws-exports.js file
 
+Amplify.configure(awsconfig);
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
