096aa537963634ed99d0df363a709cd83a1ee388
Rainzz01 <yeetianlow316@gmail.com>
2023-11-08 23:43:47 +0800

    Change Logo to png & combine all things to home page

---
+++ src/openai_js/Forms.js
@@ -0,0 +1,233 @@
+import React, { useState } from 'react';
+import { Storage } from 'aws-amplify';
+import AWS from 'aws-sdk';
+import { Col, Row } from "reactstrap";
+import {Card, CardBody} from "reactstrap";
+
+const API_KEY = process.env.REACT_APP_API_KEY; //secure -> env variable
+
+function VideoUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (selectedFile) {
+      try {
+        await Storage.put(selectedFile.name, selectedFile);
+        console.log('Video uploaded successfully.');
+      } catch (error) {
+        console.error('Error uploading video:', error);
+      }
+    }
+  };
+
+  const [fileText, setFileText] = useState('');
+  let filetimeText = '';
+  const [loadingTranscribe, setLoadingTranscribe] = useState(false);
+  const [loadingSummarized, setLoadingSummarized] = useState(false);
+
+  const handleButtonClick = async () => {
+    setLoadingTranscribe(true);
+    const AWS = require('aws-sdk');
+
+    AWS.config.update({
+      region: process.env.REACT_APP_region, // Replace with your desired region
+      accessKeyId: process.env.REACT_APP_accessKeyId, // Add your access key ID here
+      secretAccessKey: process.env.REACT_APP_secretAccessKey // Add your secret access key here
+    });
+
+    // Replace 'your-s3-bucket-name' and 'your-s3-key' with the actual bucket name and key
+    const bucketName = 'outputtranscribefromvideo';
+    const objectKey = 'VideotoTextjson123.json';
+
+    const s3 = new AWS.S3();
+    const params = {
+      Bucket: bucketName,
+      Key: objectKey
+    };
+
+    try {
+      const data = await s3.getObject(params).promise();
+      const jsonString = data.Body.toString('utf-8')
+      const jsonData = JSON.parse(jsonString);
+      const transcript = jsonData.results.transcripts[0].transcript;
+      
+      //const items = jsonData.results.items[1].start_time;
+      const items = jsonData.results.items;
+      let timestamp='';
+      let sentence='';
+      let timestampSentence='Timestamp<br>';
+
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const startTime = parseFloat(item.start_time);
+        const endTime = parseFloat(item.end_time);
+        const content = item.alternatives[0].content;
+
+        console.log(`Start Time: ${startTime}`);
+        console.log(`End Time: ${endTime}`);
+        //console.log(`Content: ${content}`);
+        if (i==0){
+          timestamp+=startTime;
+        }
+        sentence+=content+' ';
+
+        // Check if this item is the start of a new sentence 
+        if (content == ".") {
+          timestamp+=' - ';
+          const endTime = parseFloat(items[i-1].end_time);
+          timestamp+=endTime;
+          timestampSentence=timestampSentence+timestamp+' '+sentence+'<br>';
+          sentence='';
+          if (i != items.length-1){
+            const startTime = parseFloat(items[i+1].start_time);
+            timestamp=startTime;
+          }
+
+        } 
+      }
+      console.log("timestampSentence: ",timestampSentence)
+      console.log("transcript ",transcript)
+      setFileText(transcript);
+      filetimeText=timestampSentence;
+      document.getElementById('filetimeText').innerHTML=filetimeText;
+
+      callOpenAIAPI(transcript);
+    } catch (error) {
+      console.error('Error fetching file:', error);
+    }finally{
+      setLoadingTranscribe(false);
+    }
+  };
+
+
+  // useEffect(() => {
+  //   // Fetch the JSON content when the component loads (optional)
+  //   handleButtonClick();
+  // }, []);
+
+  // call openai api
+  const[summarizedText, setSummarizedText] = useState("")
+
+  async function callOpenAIAPI(text){
+    setLoadingSummarized(true);
+    console.log("call open ai api");
+    if(text == "")
+    {
+      console.log("empty text");
+    }
+    else
+    {
+      console.log("Calling the OpenAIAPI");
+
+      const APIBody = {
+          "model": "gpt-3.5-turbo",
+          "messages": [
+              {role: "user", content: "Summarize content you are provided with" + text}
+              // {role: "user", content: "Create a mindmap you are provided with in bullet point outline." + text}
+          ],
+          "temperature": 0,
+          "max_tokens": 1024,
+          "top_p":1.0,
+          "frequency_penalty":0.0,
+          "presence_penalty":0.0
+      }
+      
+      try{  
+        // await fetch("https://api.openai.com/v1/chat/completions", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type":"application/json",
+        //         "Authorization":"Bearer "+ API_KEY
+        //     },
+        //     body: JSON.stringify(APIBody)
+        // }).then((data) => {
+        //     return data.json();
+        // }).then((data) => {
+        //     console.log(data);
+        //     setSummarizedText(data.choices[0].message.content);
+        // });
+        setSummarizedText("I commented the calling open ai code to save free credit, line 101-113 in forms.js")
+      }catch(e){
+        console.log(e);
+      }finally{
+        setLoadingSummarized(false);
+      }
+    }
+  }
+
+
+  return (
+    <div>
+      <div>
+        <input type="file" accept="video/*" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload Video</button>
+        <br></br>
+        <button onClick={() => {handleButtonClick()}} disabled={loadingTranscribe || loadingSummarized}>
+          Fetch File from S3
+        </button>
+      </div>
+      {/* {console.log(loadingTranscribe || loadingSummarized)} */}
+      {loadingTranscribe && <p> loadingTranscribe...</p>}
+      {loadingSummarized && <p> loadingSummarized...</p>}
+      {fileText && 
+      <div className = "mt-3">
+        <Row>
+          <Col lg="12">
+            <Card>
+              <CardBody className="d-grid gap-3">
+                <div>
+                    <h5>Transcript:</h5>
+                    {fileText}
+                    
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div> 
+      
+      }
+
+      {/* display timestamp */}
+
+      <div>
+        <Row>
+          <Col lg="12">
+            <Card>
+              <CardBody className="d-grid gap-3">
+                <div id='filetimeText'></div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+
+      
+
+      
+
+      {summarizedText !== "" ?
+      <div>
+        <Row>
+          <Col lg="12">
+            <Card>
+              <CardBody className="d-grid gap-3">
+                <div>
+                    <h5>Summarized Text:</h5> 
+                    {summarizedText} 
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+       : null}
+    </div>
+  );
+}
+
+export default VideoUpload;
--- src/views/Starter.js
+++ src/views/Starter.js
@@ -1,127 +1,33 @@
 import { Col, Row } from "reactstrap";
 import MindMap from "../openai_js/MindMap"
-import SalesChart from "../components/dashboard/SalesChart";
-import Feeds from "../components/dashboard/Feeds";
-import ProjectTables from "../components/dashboard/ProjectTable";
-import TopCards from "../components/dashboard/TopCards";
-import Blog from "../components/dashboard/Blog";
-import bg1 from "../assets/images/bg/bg1.jpg";
-import bg2 from "../assets/images/bg/bg2.jpg";
-import bg3 from "../assets/images/bg/bg3.jpg";
-import bg4 from "../assets/images/bg/bg4.jpg";
+import VideoUpload from "../openai_js/Forms";
+import BlackLogo from "../assets/images/logos/xtremelogo.png";
 
-const BlogData = [
-  {
-    image: bg1,
-    title: "This is simple blog",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-  {
-    image: bg2,
-    title: "Lets be simple blog",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-  {
-    image: bg3,
-    title: "Don't Lamp blog",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-  {
-    image: bg4,
-    title: "Simple is beautiful",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-];
 
 const Starter = () => {
   return (
     <div>
+      <div className="container text-center">
+        <div className="row align-items-center">
+          <div className="col">
+            <img  src={BlackLogo} alt="BlackLogo"/>
+          </div>
+        </div>
+      </div>
+      {/*Upload & Summarize Button*/}
+      <Row>
+        <Col lg="12">
+          <VideoUpload />
+        </Col>
+      </Row>   
+
       {/* Generate MindMap */}
       <Row>
         <Col lg="12">
           <MindMap />
         </Col>
       </Row>      
-      {/***Top Cards***/}
-      <Row>
-        <Col sm="6" lg="3">
-          <TopCards
-            bg="bg-light-success text-success"
-            title="Profit"
-            subtitle="Yearly Earning"
-            earning="$21k"
-            icon="bi bi-wallet"
-          />
-        </Col>
-        <Col sm="6" lg="3">
-          <TopCards
-            bg="bg-light-danger text-danger"
-            title="Refunds"
-            subtitle="Refund given"
-            earning="$1k"
-            icon="bi bi-coin"
-          />
-        </Col>
-        <Col sm="6" lg="3">
-          <TopCards
-            bg="bg-light-warning text-warning"
-            title="New Project"
-            subtitle="Yearly Project"
-            earning="456"
-            icon="bi bi-basket3"
-          />
-        </Col>
-        <Col sm="6" lg="3">
-          <TopCards
-            bg="bg-light-info text-into"
-            title="Sales"
-            subtitle="Weekly Sales"
-            earning="210"
-            icon="bi bi-bag"
-          />
-        </Col>
-      </Row>
-      {/***Sales & Feed***/}
-      <Row>
-        <Col sm="6" lg="6" xl="7" xxl="8">
-          <SalesChart />
-        </Col>
-        <Col sm="6" lg="6" xl="5" xxl="4">
-          <Feeds />
-        </Col>
-      </Row>
-      {/***Table ***/}
-      <Row>
-        <Col lg="12">
-          <ProjectTables />
-        </Col>
-      </Row>
-      {/***Blog Cards***/}
-      <Row>
-        {BlogData.map((blg, index) => (
-          <Col sm="6" lg="6" xl="3" key={index}>
-            <Blog
-              image={blg.image}
-              title={blg.title}
-              subtitle={blg.subtitle}
-              text={blg.description}
-              color={blg.btnbg}
-            />
-          </Col>
-        ))}
-      </Row>
+      
     </div>
   );
 };
