419d8b6376e97ee5117da76a06b12ed9c31be1a1
Rainzz01 <yeetianlow316@gmail.com>
2023-11-10 16:40:50 +0800

    modify pre plan

--- src/views/ui/PremiumPlan.js
+++ src/views/ui/PremiumPlan.js
@@ -4,71 +4,107 @@ import { Card,
     CardText,
     Button,
  } from "reactstrap";
- import { Col, Row } from "reactstrap";
+import { Col, Row } from "reactstrap";
+import tickIcon from "../../assets/images/logos/accept.png";
+
 
 const PremiumPlan = () =>{
     return (
         <div>
             <Row style={{ display: "flex" , fontSize:"1.3rem"}}>
             <Col sm="6" style={{ flex: "1" }}>
-                <Card style={{ height: "100%", margin: "1rem", backgroundColor: "white", border: "1px solid #ddd" }}>
+                <Card style={{ height: "100%", margin: "1rem", backgroundColor: "primary", border: "4px solid #ddd" }}>
                     <CardBody style={{ padding: "2rem" }}>
-                    <CardTitle tag="h3">
-                        Basic <br />(Free)
+                    <CardTitle tag="h2" style={{ fontWeight: "bold", fontSize: "48px", lineHeight: "0.7" }}>
+                        Basic <br /><span style={{ fontSize: "24px" }}>(Free)</span>
                     </CardTitle>
                     <CardText>
-                        <ul style={{ padding: "2rem" }}>
-                        <li>Summarize videos up to 20 minutes</li>
+                        <ul style={{ padding: "2"}}>
+                            <br></br>
+                            <p>
+                                <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                Summarize videos up to 20 minutes
+                            </p>
                         <br />
-                        <li>Maximum 3 videos per day</li>
+                            <p>
+                                <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px" }} />
+                                Maximum 3 videos per day
+                            </p>
                         </ul>
                     </CardText>
                     </CardBody>
+                    <div className="d-flex justify-content-center align-items-center" style={{ height: '20vh' }}>
+                        <Button className="btn" outline color="primary" size="lg">
+                            Current Plan
+                        </Button>
+                    </div>
                 </Card>
                 </Col>
+
                 <Col sm="6" style={{ flex: "1" }}>
-                    <Card color="light" style={{height:"100%", margin:"1rem"}}>
+                    <Card color="white" style={{height:"100%", margin:"1rem"}}>
                         <CardBody style={{padding:"2rem"}}>
-                            <CardTitle tag="h3" >
-                            Pro <br></br>($8.00 per month)
+                            <CardTitle tag="h2" style={{ fontWeight: "bold", fontSize: "48px", lineHeight: "0.7" }}>
+                                Pro <br /><span style={{ fontSize: "24px" }}>($8.00 per month)</span>
                             </CardTitle>
                             <CardText>
                                 <ul style={{padding:"2rem"}}>
-                                    <li>
-                                    Summarize videos with no time limit
-                                    </li> <br></br>
-                                    <li>
-                                    Unlimited videos
-                                    </li> <br></br>
-                                    <li>
-                                    Mind map to organize and structure the information
-                                    </li> <br></br>
-                                    <li>
-                                    Flash card to aid in learning 
-                                    </li> <br></br>
+                                    <p>
+                                        <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                        Summarize videos with no time limit
+                                    </p> <br></br>
+                                    <p>
+                                        <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                        Unlimited videos
+                                    </p> <br></br>
+                                    <p>
+                                        <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                        Mind map to organize and structure the information
+                                    </p> 
+                                    <br></br>
+                                    <p style={{ marginBottom: "24px"}}>
+                                        <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                        Flash card to aid in learning 
+                                    </p>
                                 </ul>
                             </CardText>
                             <div style={{ display: "flex", justifyContent: "center" }}>
-                                <Button color="light-primary">Upgrade now!</Button>
+                                <Button className="btn" color="primary" size="lg">
+                                    Upgrade now!
+                                </Button>
                             </div>
                         </CardBody>
                     </Card>
                 </Col>
+
                 <Col sm="6" style={{ flex: "1" }}>
-                    <Card color="warning" style={{height:"100%" , margin:"1rem"}}> 
+                    <Card color="white" style={{height:"100%" , margin:"1rem"}}> 
                         <CardBody style={{padding:"2rem"}}>
-                            <CardTitle tag="h3">
-                            Premium <br></br>($20.00 per month)
+
+                            <CardTitle tag="h2" style={{ fontWeight: "bold", fontSize: "48px", lineHeight: "0.7" }}>
+                               Premium <br /><span style={{ fontSize: "24px" }}>($20.00 per month)</span>
                             </CardTitle>
+
                             <CardText>
                                 <ul style={{padding:"2rem"}}>
-                                    <li>All features from Pro</li><br></br>
-                                    <li>More accurate summarization powered by latest AI model!</li>
+                                    <p>
+                                        <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                        All features from Pro
+                                    </p>
+                                    <br></br>
+                                    <p style={{ marginBottom: "245px"}}>
+                                        <img src={tickIcon} alt="Tick" style={{ width: "20px", marginRight: "7px"}} />
+                                        More accurate summarization powered by latest AI model!
+                                    </p>
                                 </ul>
                             </CardText>
-                            <div style={{ display: "flex", justifyContent: "center" }}>
-                                <Button color="light-primary">Upgrade now!</Button>
+
+                            <div style={{ display: "flex", justifyContent: "center"}}>
+                                <Button className="btn" color="primary" size="lg">
+                                    Upgrade now!
+                                </Button>
                             </div>
+                            
                         </CardBody>
                     </Card>
                 </Col>
