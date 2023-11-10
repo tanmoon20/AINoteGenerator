f0e97533f0fb46bc32fb03bd1322e2051ee092c9
tanmoon20 <tanseemoon@gmail.com>
2023-11-10 20:07:30 +0800

    remove 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React, { Suspense } from "react";
 import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 import {Amplify} from 'aws-amplify';
@@ -22,8 +21,3 @@ root.render(
     </HashRouter>
   </Suspense>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
